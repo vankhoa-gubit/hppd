3906ca071953e7b19b4370b2740d5ccfe8b78965
Văn Khoa <khoapvan204@gmail.com>
2025-10-24 18:51:33 +0700

    Remove unnecessary blank line in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig(({ command }) => {
   const base = command === 'build' ? '/hppd/' : '/'
-  
+
   return {
     plugins: [react(), tailwindcss()],
     base,
